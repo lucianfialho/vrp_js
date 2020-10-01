ee5c89706745fa3a8cdc570c52bfa3cfa5407b5a
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-10-01 12:19:43 -0300

    -- adicionando loading screen

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -13,3 +13,18 @@ server_scripts {
     'database/connect.js',
     'base.js'
 }
+
+
+
+client_scripts {
+	"wait.js",
+	"client/player_state.js",
+}
+
+files {
+	"loading/index.html",
+	"loading/background.jpg",
+	"loading/logo.png",
+	"loading/css.css",
+}
+loadscreen "loading/index.html"
