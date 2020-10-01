74f3da1bc43e611025d3c7b75d25427d4e95cb0b
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-10-01 12:18:20 -0300

    -- Mudando o uso do mongoose para async await

--- base.js
+++ base.js
@@ -1,6 +1,5 @@
 const UserModel = require('./database/model/User')
 const CharacterModel = require('./database/model/Character');
-const Character = require('./database/model/Character');
 
 const getsteamIdentifier = () => {
     
@@ -19,42 +18,42 @@ const getsteamIdentifier = () => {
     return steamIdentifier
 }
 
+(async ()=>{
+    on('playerConnecting', async(name, setKickReason, deferrals) => {
+        deferrals.defer()
+        
+        deferrals.update(`Olá ${name}. Estamos confirmando seu STEAMID.`)
+        
+        let source = global.source
 
-on('playerConnecting', (name, setKickReason, deferrals) => {
-    deferrals.defer()
-    
-    deferrals.update(`Olá ${name}. Estamos confirmando seu STEAMID.`)
-    
-    let source = global.source
-    
-    const steamIdentifier = getsteamIdentifier();
-    
-    UserModel.findOne({ steamId: steamIdentifier }, (err, result) => {
-        deferrals.update(`Procurando seu usuário na nossa base`)
-        if(!result) {
+        const steamIdentifier = getsteamIdentifier();    
+        const User =  await UserModel.findOne({ steamId: steamIdentifier }).exec()
+
+        if(!User) {
             deferrals.update(`Usuário não encontrado, criando usuário`)
            
-            const user = new UserModel({ 
+            const User = new UserModel({ 
                 steamId: steamIdentifier,
                 queue_priority: 0,
                 banned: false,
                 bannedBy: "Não afetado.",
                 personLimit: 1
             })
-
-            user.save(function (err) {
+    
+            User.save(function (err) {
                 if (err) return deferrals.update(`Deu merda passa esse erro para um administrador: ${err}`)
-                deferrals.update(`Usuário criado com sucesso`)
+                deferrals.done(`Usuário criado com sucesso`)
             })
-
+    
         } else {
             deferrals.update(`Achamos você aqui agora aguarda um pouco que eu vou puxar seus personagens`)
-            
-            if(result.banned) return deferrals.done(`Brow tu come merda? Tu chitou no bonde do cebola e quer aparecer aqui com essa cara lavada, vai dar esse cu!`)
-            if(!result.whitelisted) return deferrals.done(`Parece que é seu primeiro login envie para a equipe de administração o seu ID: ${result._id}`)
 
+            if(User.banned) return deferrals.done(`Brow tu come merda? Tu chitou no bonde do cebola e quer aparecer aqui com essa cara lavada, vai dar esse cu!`)
+            if(!User.whitelisted) return deferrals.done(`Parece que é seu primeiro login envie para a equipe de administração o seu ID: ${result._id}`)
+    
             deferrals.done()
-
+    
         }
     });
-});
+    
+})()
