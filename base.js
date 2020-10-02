61f3764206a908a3cca60455a9aad778bafafe2d
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-10-02 11:18:56 -0300

    -- bugfix alterando variavel de callback

--- base.js
+++ base.js
@@ -21,7 +21,7 @@ const getsteamIdentifier = () => {
 ( ()=>{
     on('playerConnecting', async(name, setKickReason, deferrals) => {
         deferrals.defer()
-        
+
         deferrals.update(`Olá ${name}. Estamos confirmando seu STEAMID.`)
         
         let source = global.source
@@ -41,17 +41,20 @@ const getsteamIdentifier = () => {
             })
     
             User.save(function (err) {
+
                 if (err) return deferrals.update(`Deu merda passa esse erro para um administrador: ${err}`)
+                
+                emit("vRP:playerJoin", User, source)
                 deferrals.done(`Usuário criado com sucesso`)
             })
     
         } else {
+
             deferrals.update(`Achamos você aqui agora aguarda um pouco que eu vou puxar seus personagens`)
 
             if(User.banned) return deferrals.done(`Brow tu come merda? Tu chitou no bonde do cebola e quer aparecer aqui com essa cara lavada, vai dar esse cu!`)
-            if(!User.whitelisted) return deferrals.done(`Parece que é seu primeiro login envie para a equipe de administração o seu ID: ${result._id}`)
-    
-            deferrals.done()
+
+            if(!User.whitelisted) return deferrals.done(`Parece que é seu primeiro login envie para a equipe de administração o seu ID: ${User._id}`)
     
         }
     });
