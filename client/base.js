94ee2a7ec4b7bcefea02a0b0fca13ddaf806fe74
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-10-17 14:21:16 -0300

    -- spawnando o usuario

--- client/base.js
+++ client/base.js
@@ -1,51 +1,31 @@
+(() => {
 
-const getUserId = (source) => {
-	if(!source) {
-		const playerIdentifiers = GetPlayerIdentifiers(source)
-		console.log(playerIdentifiers)
-	}
-}
+	global.on(
+		'onClientGameTypeStart', 
+		async (resourceName) => {
+			await emitNet('vRP:onClientGameTypeStart')
+			
+		}
+	)
 
-const getPosition = () => {
-    const {x, y, z} = GetEntityCoords(PlayerPedId(),true)
-	return x,y,z
-}
 
-const isInside = () => {
-    const {x, y, z} = tvRP.getPosition()
-    return !(GetInteriorAtCoords(x,y,z) == 0)
-}
+	global.onNet(
+		'cliOnClientGameTypeStart', 
+		async (characterPosition) => {
+			exports.spawnmanagerjs.setAutoSpawnCallback(() => {
+				exports.spawnmanagerjs.spawnPlayer({
+					x: characterPosition.x, // characterPosition.x
+					y: characterPosition.y, // characterPosition.y
+					z: characterPosition.z, // characterPosition.z
+					model: 'a_m_y_epsilon_02'
+				}, (spawn) => {
+					console.log(spawn)
+				})
+			})
+		
+			exports.spawnmanagerjs.setAutoSpawn(true)
+			exports.spawnmanagerjs.forceRespawn()
+		}
+	)
+})()
 
-const getCamDirection = () => {
-	const heading = GetGameplayCamRelativeHeading() + GetEntityHeading(PlayerPedId())
-	const pitch = GetGameplayCamRelativePitch()
-	const x = -math.sin(heading*math.pi/180.0)
-	const y = math.cos(heading*math.pi/180.0)
-	const z = math.sin(pitch*math.pi/180.0)
-	const len = math.sqrt(x*x+y*y+z*z)
-    
-    if (len !== 0) {
-		x = x/len
-		y = y/len
-		z = z/len
-	}
-
-    return x, y, z
-}
-
-const getNearestPlayers = (radius) => {
-	const r = {}
-	const ped = GetPlayerPed(i)
-	const pid = PlayerId()
-	const px, py, pz = tvRP.getPosition()
-
-    
-}
-
-module.exports = {
-    getPosition,
-    isInside,
-    getCamDirection,
-	getNearestPlayers,
-	getUserId,
-}
