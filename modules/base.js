91ada83853bad1d24ed7533dbe1074da59f7e6d4
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-10-17 14:18:58 -0300

    -- adicionando posição do usuario

---
+++ modules/base.js
@@ -0,0 +1,29 @@
+(() => {
+   
+    const UserModel = require('./database/model/User');
+    const PositionModel = require('./database/model/Position');
+
+    const getUser = async() => {
+        const steamIdentifier = getsteamIdentifier()
+        return await UserModel.findOne({ steamId: steamIdentifier }).populate('characters').exec()
+    }
+
+    const getCharacterPosition = async(characterId) => {
+        return await PositionModel.findOne({ character: characterId }).exec()
+    }
+
+    global.onNet(
+        'vRP:onClientGameTypeStart',
+        async () => {
+            let source = global.source
+            const user = await getUser();
+            const characterPosition = await getCharacterPosition(user.characters[0]._id)
+            const position = {
+                x: characterPosition.x,
+                y: characterPosition.y,
+                z: characterPosition.z
+            }
+            global.emitNet("cliOnClientGameTypeStart", source, position);
+        }
+    )
+})()
